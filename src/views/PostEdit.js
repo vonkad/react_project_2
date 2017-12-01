d759f425f7795d4878948479b5ee576987fe2f78
David Vonka (dell linux) <david.vonka@gmail.com>
2017-12-01 20:28:16 +0100

    crap

--- src/views/PostEdit.js
+++ src/views/PostEdit.js
@@ -4,13 +4,30 @@ import ListOfCategories from "../components/ListOfCategories";
 import ListOfPosts from "../components/ListOfPosts";
 import SortingListOfPosts from "../components/SortingListOfPosts";
 import AddAPostButton from "../components/AddAPostButton";
+import {connect} from "react-redux";
 
-export default class PostEdit extends Component {
+class PostEdit extends Component {
 
     render() {
         const postId = this.props.match.params.postid;
-        return <div>
-            {postId}
-        </div>
+        const state = this.props.state;
+        if (state==='ready') {
+            const posts = this.props.posts;
+            return <div>{
+                posts.filter(post => post.id === postId).map(post => <div>{post.title}</div>)
+            }</div>
+        } else {
+            return <div>Loading...</div>
+        }
     }
 }
+
+function mapStateToProps({posts}) {
+    return posts;
+}
+
+function mapDispatchToProps(dispatch) {
+    return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostEdit);
